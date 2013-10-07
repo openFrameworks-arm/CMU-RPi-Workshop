c6ad6c4c8b2a6f1751d154a680b1b76f5024647d
Jason Van Cleave <jvcleave@gmail.com>
2013-10-07 18:28:39 -0400

    added drawingExample

--- drawingExample/src/ofApp.cpp
+++ drawingExample/src/ofApp.cpp
@@ -1,18 +1,320 @@
 #include "ofApp.h"
 
+
 //--------------------------------------------------------------
 void ofApp::setup(){
-
+	//ofSetVerticalSync(false);
+	image.loadImage("of.png");
+	plane.set( image.getWidth(), image.getHeight());
+	sphere.setRadius(image.getWidth());
+	sphere.mapTexCoordsFromTexture(image.getTextureReference());
+	ofEnableSmoothing();
+	ofEnableAntiAliasing();
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
+	//ofSetColor(ofColor::red);
+	
+}
 
+int x = 0;
+int y = 0;
+int gridSize =1;
+
+
+
+void drawMoving()
+{
+	ofPushStyle();
+	
+	x+=10;
+	if (x>ofGetWidth()) 
+	{
+		x=0;
+		y+=100;
+		if (y>ofGetHeight()) 
+		{
+			y=0;
+		}
+	}
+	ofNoFill();
+	/*
+	 white, gray, black, red, green, blue, cyan, magenta,
+	 yellow,aliceBlue,antiqueWhite,aqua,aquamarine,azure,beige,bisque,blanchedAlmond,
+	 blueViolet,brown,burlyWood,cadetBlue,chartreuse,chocolate,coral,cornflowerBlue,cornsilk,
+	 crimson,darkBlue,darkCyan,darkGoldenRod,darkGray,darkGrey,darkGreen,darkKhaki,
+	 darkMagenta,darkOliveGreen,darkorange,darkOrchid,darkRed,darkSalmon,darkSeaGreen,
+	 darkSlateBlue,darkSlateGray,darkSlateGrey,darkTurquoise,darkViolet,deepPink,
+	 deepSkyBlue,dimGray,dimGrey,dodgerBlue,fireBrick,floralWhite,forestGreen,fuchsia,
+	 gainsboro,ghostWhite,gold,goldenRod,grey,greenYellow,honeyDew,hotPink,indianRed,indigo,
+	 ivory,khaki,lavender,lavenderBlush,lawnGreen,lemonChiffon,lightBlue,lightCoral,
+	 lightCyan,lightGoldenRodYellow,lightGray,lightGrey,lightGreen,lightPink,lightSalmon,
+	 lightSeaGreen,lightSkyBlue,lightSlateGray,lightSlateGrey,lightSteelBlue,lightYellow,
+	 lime,limeGreen,linen,maroon,mediumAquaMarine,mediumBlue,mediumOrchid,mediumPurple,
+	 mediumSeaGreen,mediumSlateBlue,mediumSpringGreen,mediumTurquoise,mediumVioletRed,
+	 midnightBlue,mintCream,mistyRose,moccasin,navajoWhite,navy,oldLace,olive,oliveDrab,
+	 orange,orangeRed,orchid,paleGoldenRod,paleGreen,paleTurquoise,paleVioletRed,papayaWhip,
+	 peachPuff,peru,pink,plum,powderBlue,purple,rosyBrown,royalBlue,saddleBrown,salmon,
+	 sandyBrown,seaGreen,seaShell,sienna,silver,skyBlue,slateBlue,slateGray,slateGrey,snow,
+	 springGreen,steelBlue,blueSteel,tan,teal,thistle,tomato,turquoise,violet,wheat,whiteSmoke,
+	 yellowGreen;
+	 */
+	ofSetColor(ofColor::fireBrick);
+	ofDrawSphere(x, y, 100);
+	ofPopStyle();
+	
+	ofPushMatrix();
+	ofTranslate(x, y);
+	ofCircle(0, 0, 100);
+	ofDrawGrid(gridSize);
+	ofPopMatrix();
+	
+}
+int spin = 0;
+
+ofColor getRandomColor()
+{
+	return ofColor(ofRandom(255), ofRandom(255), ofRandom(255));
+}
+
+void drawCurve()
+{
+	ofPoint pt1(0, 0);
+	ofPoint pt2(0, 100);
+	ofPoint pt3(100, 100);
+	ofPoint pt4(100, 0);
+	
+
+	ofPushStyle();
+		ofNoFill();
+		ofSetColor(ofColor::red);
+		ofCurve(pt1.x, pt1.y,
+				pt2.x, pt2.y,
+				pt3.x, pt3.y,
+				pt4.x, pt4.y
+				);
+		
+		//ofSetColor(getRandomColor());
+		ofSetColor(ofColor::blue);
+		ofCircle(pt1, 10);
+		ofCircle(pt2, 10);
+		ofCircle(pt3, 10);
+		ofCircle(pt4, 10);
+	ofPopStyle();
+}
+
+void drawShapeFromPoints()
+{
+	ofPoint pt1(0, 0);
+	ofPoint pt2(0, 100);
+	ofPoint pt3(100, 100);
+	ofPoint pt4(100, 0);
+	
+	
+	ofPushStyle();
+		ofNoFill();
+		ofSetColor(ofColor::black);
+		ofBeginShape();
+			ofVertex(pt1);
+			ofVertex(pt2);
+			ofVertex(pt3);
+			ofVertex(pt4);
+			bool doCloseShape = (ofGetFrameNum()%100==0);
+		ofEndShape(doCloseShape);//bool is option to close shape
+	ofPopStyle();
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
+	
+	int labelY = -20; //
+	
+	//Curves
+	ofPushMatrix();
+		ofTranslate(100, 50);
+			ofDrawBitmapString("ofCurve", 0, labelY);
+			drawCurve();
+	ofPopMatrix();
+	
+	//Shapes from Points
+	ofPushMatrix();
+		ofTranslate(300, 50);
+			ofDrawBitmapString("ofBeginShape\nofEndShape", 0, labelY);
+			drawShapeFromPoints();
+	ofPopMatrix();
+	
+	//ofRect
+	ofPushMatrix();
+		ofTranslate(500, 50);
+			ofDrawBitmapString("ofRectangle", 0, labelY);
+			ofRectangle rectangle(0, 0, 100, 100);
+			ofRect(rectangle);
+	ofPopMatrix();
+	
+	//ofRectRounded
+	ofPushMatrix();
+	ofTranslate(700, 50);
+		//% is a cool way of keeping a number within range
+		float cornerRadius = (float) (ofGetFrameNum()%40);
+		ofDrawBitmapString("ofRectRounded", 0, labelY);
+		ofRectRounded(rectangle, cornerRadius);
+	ofPopMatrix();
+	
+	//ofCircle
+	ofPushMatrix();
+		ofTranslate(900, 50);
+		
+		stringstream circleInfo;
+		int circleResolution = ofGetFrameNum()%48;
+		ofSetCircleResolution(circleResolution);
+		circleInfo << "ofCircle" << "\n" << "Circle Resolution: " << circleResolution;
+		ofDrawBitmapString(circleInfo.str(), 0, labelY);
+		ofCircle(50, 50, 50);
+	ofPopMatrix();
+	
+	//ofEllipse
+	ofPushMatrix();
+		ofTranslate(1100, 50);
+		ofDrawBitmapString("ofEllipse", 0, labelY);
+		ofEllipse(50, 50, 50, 100);
+	ofPopMatrix();
+	
+	//ofDrawBox
+	ofPushMatrix();
+		ofTranslate(ofGetFrameNum()%ofGetWidth()/2, 250);
+		ofDrawBitmapString("ofDrawBox", 50, labelY);
+		ofNoFill();
+		ofRotateY(ofGetFrameNum()%360);
+		ofDrawBox(0, 0, 0, 50, 50, 50);
+	ofPopMatrix();
+	
+	
+	//ofDrawSphere
+	ofPushMatrix();
+		ofTranslate(ofGetFrameNum()%ofGetWidth()/2, 350);
+		stringstream info;
+		info << "ofDrawSphere" << "\n" << "ofGetSphereResolution: " << ofGetSphereResolution();
+		ofDrawBitmapString(info.str(), 70, labelY);
+		ofNoFill();
+		ofRotateY(ofGetFrameNum()%360);
+		//Sphere Resolution default is 20
+		ofSetSphereResolution(ofGetFrameNum()%40);
+		ofDrawSphere(0, 0, 0, 50);
+	ofPopMatrix();
+	
+	//ofDrawCone
+	ofPushMatrix();
+		ofTranslate(ofGetFrameNum()%ofGetWidth()/2, 450);
+		ofDrawBitmapString("ofDrawCone", 70, labelY);
+		ofNoFill();
+		ofRotateY(ofGetFrameNum()%360);
+		//Cone Resolution default is 9, 3, 2
+		//ofSetConeResolution(ofGetFrameNum()%18, 3, 2);
+		ofDrawCone(0, 0, 0, 50, 50);
+	ofPopMatrix();
+	
+	//ofDrawCylinder
+	ofPushMatrix();
+		ofTranslate(ofGetFrameNum()%ofGetWidth()/2, 550);
+		ofDrawBitmapString("ofDrawCylinder", 70, labelY);
+		ofNoFill();
+		ofRotateY(ofGetFrameNum()%360);
+		//Cylinder Resolution default is 8, 4, 2
+		//ofSetCylinderResolution(ofGetFrameNum()%16, 4, 2);
+		ofDrawCylinder(0, 0, 0, 20, 50);
+	ofPopMatrix();
+	
+	//ofDrawIcoSphere
+	ofPushMatrix();
+		ofTranslate(ofGetFrameNum()%ofGetWidth()/2, 650);
+		ofDrawBitmapString("ofDrawIcoSphere", 70, labelY);
+		ofNoFill();
+		ofRotateY(ofGetFrameNum()%360);
+		//http://blender.stackexchange.com/questions/72/what-is-the-difference-between-a-uv-sphere-and-an-icosphere
+		ofDrawIcoSphere(0, 0, 0, 50);
+	ofPopMatrix();
+	
+	
+	//ofDrawPlane
+	ofPushMatrix();
+		ofTranslate(ofGetWidth()/2, 0);	//start at center
+		ofTranslate(ofGetFrameNum()%ofGetWidth()/2, 250); //center is 0, 0 so we increment from there
+			ofDrawBitmapString("ofDrawPlane", 70, labelY);
+			ofFill();
+			ofRotateY(ofGetFrameNum()%360);
+			ofDrawPlane(0, 0, 50, 50);
+	ofPopMatrix();
+	
+	
+	//ofDrawGrid
+	ofPushMatrix();
+		ofTranslate(ofGetWidth()/2, 0);	//start at center	
+		ofTranslate(ofGetFrameNum()%ofGetWidth()/2, 350); //center is 0, 0 so we increment from there
+		ofDrawBitmapString("ofDrawGrid", 70, labelY);
+		//void ofDrawGrid(float scale = 10.0f, float ticks = 8.0f, bool labels = false, bool x = true, bool y = true, bool z = true)
+		ofRotateY(ofGetFrameNum()%360);
+		ofDrawGrid(50, 8.0f, false, true, true, true);
+		
+	ofPopMatrix();
+	
+	
+	//ofSetColor(ofColor::red);
+	//ofCircle(0, 0, 100);
+	
+	//ofEllipse
+	//ofLine
+	//ofRect(0, 0, 100, 100);
+	
+	/*ofRectangle rectangle(0, 0, 100, 100);
+	ofRect(rectangle);
+	
+	ofRectRounded(rectangle, 1.0);*/
+	
+		//void ofDrawGrid(float scale = 10.0f, float ticks = 8.0f, bool labels = false, bool x = true, bool y = true, bool z = true)
+	/*ofPushMatrix();
+		ofTranslate(ofGetWidth()/2, ofGetHeight()/2);
+		gridSize++;
+		if(gridSize>ofGetHeight()/2)
+		{
+			gridSize = 1;
+		}
+		
+			ofRect(0, 0, gridSize, gridSize);
+			//ofDrawBitmapString("OF_RECTMODE_CORNER", 0, 0);
+			ofDrawGrid(gridSize, 8.0f, false, false, false, true);
+			image.draw(0, 0);
+		
+		//ofDrawGridPlane(gridSize);
+	ofPopMatrix();*/
+	/*int quarterScreenWidth = ofGetWidth()/4;
+	int quarterScreenHeight= ofGetHeight()/4;
+	
+	spin++;
+	if(spin>360) spin = 0;
+	
+	ofPushMatrix();
+		ofTranslate(quarterScreenWidth, quarterScreenHeight);
+		ofRotateY(spin);
+		image.bind();
+			plane.drawFaces();
+		image.unbind();
+	ofPopMatrix();
+	
+	ofPushMatrix();
+		ofTranslate(quarterScreenWidth*2, quarterScreenHeight);
+		image.bind();
+			sphere.drawFaces();
+		image.unbind();
+	ofPopMatrix();
+	
+	ofPushMatrix();
+		ofTranslate(quarterScreenWidth*3, quarterScreenHeight);
+		ofRotateY(spin);
+		image.bind();
+			sphere.drawFaces();
+		image.unbind();
+	ofPopMatrix();*/
 }
 
 //--------------------------------------------------------------
