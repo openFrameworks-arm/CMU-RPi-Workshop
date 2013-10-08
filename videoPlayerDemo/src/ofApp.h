786f5ee655bf8fc5b5fb1db808cf96fe7bcf58ed
Jason Van Cleave <jvcleave@gmail.com>
2013-10-08 01:10:49 -0400

    added videoPlayerDemo

--- videoPlayerDemo/src/ofApp.h
+++ videoPlayerDemo/src/ofApp.h
@@ -1,20 +1,31 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxOpenCv.h"
+
 
 class ofApp : public ofBaseApp{
+
 	public:
 		void setup();
 		void update();
-		void draw();
-		
+		void draw();		
 		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y);
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
+
+		ofVideoPlayer			videoPlayer;
+		int						videoWidth;
+		int						videoHeight;
+	
+        ofxCvColorImage			colorImage;
+        ofxCvGrayscaleImage 	grayImage;
+		ofxCvGrayscaleImage 	backgroundImage;
+		ofxCvGrayscaleImage 	differenceImage;
+
+        ofxCvContourFinder		contourFinder;
+	
+		int						thresholdAmount;
+		bool					doLearnBackground;
+		
+		ofTrueTypeFont			font;
 };
+
