3d36af16935c5febf9be5922b78522635202c6a2
Jason Van Cleave <jvcleave@gmail.com>
2013-10-10 12:27:42 -0400

    corrected update script path to OF

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-git clone https://github.com/bakercp/ofxMediaType.git /homes/pi/openFrameworks/addons/ofxMediaType 2> /dev/null
-git clone https://github.com/bakercp/ofxSMTP.git /homes/pi/openFrameworks/addons/ofxSMTP 2> /dev/null
-git clone https://github.com/bakercp/ofxHTTP.git /homes/pi/openFrameworks/addons/ofxHTTP 2> /dev/null
-git clone https://github.com/bakercp/ofxJSON.git /homes/pi/openFrameworks/addons/ofxJSON 2> /dev/null
-git clone https://github.com/bakercp/ofxIpVideoGrabber.git /homes/pi/openFrameworks/addons/ofxIpVideoGrabber 2> /dev/null
+git clone https://github.com/bakercp/ofxMediaType.git /home/pi/openFrameworks/addons/ofxMediaType 2> /dev/null
+git clone https://github.com/bakercp/ofxSMTP.git /home/pi/openFrameworks/addons/ofxSMTP 2> /dev/null
+git clone https://github.com/bakercp/ofxHTTP.git /home/pi/openFrameworks/addons/ofxHTTP 2> /dev/null
+git clone https://github.com/bakercp/ofxJSON.git /home/pi/openFrameworks/addons/ofxJSON 2> /dev/null
+git clone https://github.com/bakercp/ofxIpVideoGrabber.git /home/pi/openFrameworks/addons/ofxIpVideoGrabber 2> /dev/null
 
-git clone https://github.com/bakercp/ofSketch.git /homes/pi/openFrameworks/apps/ofSketch 2> /dev/null
-git clone https://github.com/openFrameworks-arm/CMU-RPi-Workshop.git /homes/pi/openFrameworks/apps/CMU-RPi-Workshop 2> /dev/null
+git clone https://github.com/bakercp/ofSketch.git /home/pi/openFrameworks/apps/ofSketch 2> /dev/null
+git clone https://github.com/openFrameworks-arm/CMU-RPi-Workshop.git /home/pi/openFrameworks/apps/CMU-RPi-Workshop 2> /dev/null
 
-cd /homes/pi/openFrameworks/addons/ofxMediaType && git pull && cd -
-cd /homes/pi/openFrameworks/addons/ofxSMTP && git pull && cd -
-cd /homes/pi/openFrameworks/addons/ofxHTTP && git pull && cd -
-cd /homes/pi/openFrameworks/addons/ofxJSON && git pull && cd -
-cd /homes/pi/openFrameworks/addons/ofxIpVideoGrabber && git pull && cd -
+cd /home/pi/openFrameworks/addons/ofxMediaType && git pull && cd -
+cd /home/pi/openFrameworks/addons/ofxSMTP && git pull && cd -
+cd /home/pi/openFrameworks/addons/ofxHTTP && git pull && cd -
+cd /home/pi/openFrameworks/addons/ofxJSON && git pull && cd -
+cd /home/pi/openFrameworks/addons/ofxIpVideoGrabber && git pull && cd -
 
-cd /homes/pi/openFrameworks/apps/ofSketch && git pull && cd -
-cd /homes/pi/openFrameworks/apps/CMU-RPi-Workshop && git pull && cd -
+cd /home/pi/openFrameworks/apps/ofSketch && git pull && cd -
+cd /home/pi/openFrameworks/apps/CMU-RPi-Workshop && git pull && cd -
 
