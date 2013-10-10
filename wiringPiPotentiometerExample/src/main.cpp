acae7dc5853eccf2c66fb3e2cd25373efa5cee3c
Jason Van Cleave <jvcleave@gmail.com>
2013-10-09 20:54:16 -0400

    wiringPiPotentiometerExample complete - needs check on final SD

--- wiringPiPotentiometerExample/src/main.cpp
+++ wiringPiPotentiometerExample/src/main.cpp
@@ -1,20 +1,11 @@
 #include "ofMain.h"
 #include "testApp.h"
-#ifdef TARGET_OPENGLES
 #include "ofGLProgrammableRenderer.h"
-#endif
-//========================================================================
-int main( ){
 
+int main()
+{
 	ofSetLogLevel(OF_LOG_VERBOSE);
-	#ifdef TARGET_OPENGLES
-	ofSetCurrentRenderer(ofPtr<ofBaseRenderer>(new ofGLProgrammableRenderer()));
-	#endif
-	ofSetupOpenGL(1280,720, OF_WINDOW);			// <-------- setup the GL context
-
-	// this kicks off the running of my app
-	// can be OF_WINDOW or OF_FULLSCREEN
-	// pass in width and height too:
+	ofSetCurrentRenderer(ofGLProgrammableRenderer::TYPE);
+	ofSetupOpenGL(1280, 720, OF_WINDOW);
 	ofRunApp( new testApp());
-
 }
