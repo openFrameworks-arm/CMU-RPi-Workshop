648add2489d69b2ed9ac81f16ed1a468df521754
Jason Van Cleave <jvcleave@gmail.com>
2013-10-10 14:55:01 -0400

    no homes

--- scripts/buildAddons.sh
+++ scripts/buildAddons.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo Compiling addon examples with MAKEFLAGS=${MAKEFLAGS}
-find /homes/pi/openFrameworks/addons/*/*example* -name 'Makefile' -execdir make \;
+find /home/pi/openFrameworks/addons/*/*example* -name 'Makefile' -execdir make \;
 
 
