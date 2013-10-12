418c632b35057acbb38628a0f8598e0411c67102
Christopher Baker <me@christopherbaker.net>
2013-10-12 12:04:01 -0400

    RPI Hack.

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone https://github.com/bakercp/ofxMediaType.git /home/pi/openFrameworks/addons/ofxMediaType 2> /dev/null
+#git clone https://github.com/bakercp/ofxMediaType.git /home/pi/openFrameworks/addons/ofxMediaType 2> /dev/null
 git clone https://github.com/bakercp/ofxSMTP.git /home/pi/openFrameworks/addons/ofxSMTP 2> /dev/null
 git clone https://github.com/bakercp/ofxHTTP.git /home/pi/openFrameworks/addons/ofxHTTP 2> /dev/null
 git clone https://github.com/bakercp/ofxJSON.git /home/pi/openFrameworks/addons/ofxJSON 2> /dev/null
@@ -14,7 +14,7 @@ git clone https://github.com/bakercp/ofxLibmagic.git /home/pi/openFrameworks/add
 git clone https://github.com/bakercp/ofSketch.git /home/pi/openFrameworks/apps/ofSketch 2> /dev/null
 git clone https://github.com/openFrameworks-arm/CMU-RPi-Workshop.git /home/pi/openFrameworks/apps/CMU-RPi-Workshop 2> /dev/null
 
-cd /home/pi/openFrameworks/addons/ofxMediaType && git pull && cd -
+#cd /home/pi/openFrameworks/addons/ofxMediaType && git pull && cd -
 cd /home/pi/openFrameworks/addons/ofxSMTP && git pull && cd -
 cd /home/pi/openFrameworks/addons/ofxHTTP && git pull && cd -
 cd /home/pi/openFrameworks/addons/ofxJSON && git pull && cd -
@@ -28,3 +28,4 @@ cd /home/pi/openFrameworks/addons/ofxLibmagic && git pull && cd -
 cd /home/pi/openFrameworks/apps/ofSketch && git pull && cd -
 cd /home/pi/openFrameworks/apps/CMU-RPi-Workshop && git pull && cd -
 
+cd /home/pi/openFrameworks/apps/CMU-RPi-Workshop/scripts && ./buildAddons.sh  && cd -
